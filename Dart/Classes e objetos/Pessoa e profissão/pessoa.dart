4f430f8b7c264ecfcb818cb9b3dc295c4856c0cd
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2022-10-12 15:14:27 -0300

    Criação da classe Pessoa - Dart

---
+++ Dart/Classes e objetos/Pessoa e profissão/pessoa.dart
@@ -0,0 +1,12 @@
+class Pessoa {
+  // Atributos da classe Pessoa(aquilo que caracteriza uma pessoa)
+
+  // Em Dart, as variáveis precisam ser inicializadas por conta do null-safety(variáveis não podem ser nulas)
+
+  String nomeDaPessoa = '';
+  String cpfDaPessoa = '';
+
+  // Para exemplificar a definição de atributos da classe individualmente, esta classe possui, apenas, o construtor padrão, que não é necessário criá-lo.
+
+  Pessoa() {}
+}

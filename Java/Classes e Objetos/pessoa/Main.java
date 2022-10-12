662a8fee84631a8f26cf7704d39c507188ccb799
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2022-10-12 16:14:39 -0300

    Classes e objetos Java: Main - Pessoa

---
+++ Java/Classes e Objetos/pessoa/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Instância dos objetos
+        Pessoa primeiraPessoa = new Pessoa();
+        Profissao profissao = new Profissao("professor", "matutino");
+
+        // Definição de atributos do objeto primeiraPessoa
+        primeiraPessoa.nomeDaPessoa = "André";
+        primeiraPessoa.cpfDaPessoa = "123.456.789-10";
+
+        System.out.print("\n" + primeiraPessoa.nomeDaPessoa + ", de CPF" + primeiraPessoa.cpfDaPessoa + ", trabalha como " + profissao.nomeDaProfissao + " no turno " + profissao.turnoDoExpediente + ".\n");
+    }
+}

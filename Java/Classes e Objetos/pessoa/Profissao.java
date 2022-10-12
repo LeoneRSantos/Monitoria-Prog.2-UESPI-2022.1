8d0046537623b785c7da11885c72d376bf5ef2d1
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2022-10-12 16:14:18 -0300

    Classes e objetos Java: classe Profissao

---
+++ Java/Classes e Objetos/pessoa/Profissao.java
@@ -0,0 +1,16 @@
+public class Profissao {
+    // Atributos da classe Profissão(aquilo que caracteriza uma Profissão)
+
+    String nomeDaProfissao;
+    String turnoDoExpediente;
+
+    Profissao(){}
+    // Para exemplificar o uso de construtores com parâmetros(já definem os
+    // atributos do objeto no momento da instanciação), esta classe possui um
+    // construtor com os atributos já citados.
+
+    Profissao(String nome, String turno) {
+        this.nomeDaProfissao = nome;
+        this.turnoDoExpediente = turno;
+    }
+}

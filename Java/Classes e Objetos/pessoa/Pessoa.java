0b03b41878615a529d6b717d276a6a395d7fb9d5
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2022-10-12 16:14:04 -0300

    Classes e objetos Java: classe Pessoa

---
+++ Java/Classes e Objetos/pessoa/Pessoa.java
@@ -0,0 +1,16 @@
+/**
+ * @author Leone Rodrigues Santos
+ *         Pessoa
+ */
+public class Pessoa {
+    // Atributos da classe Pessoa(aquilo que caracteriza uma pessoa)
+
+    String nomeDaPessoa;
+    String cpfDaPessoa;
+
+    // Para exemplificar a definição de atributos da classe individualmente, esta
+    // classe possui, apenas, o construtor padrão.
+
+    Pessoa() {
+    }
+}

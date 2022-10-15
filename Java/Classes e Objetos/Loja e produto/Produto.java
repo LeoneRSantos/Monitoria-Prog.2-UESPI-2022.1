9941947ca12d5a3f32043144e6e08aeacc8f1e37
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2022-10-15 19:46:13 -0300

    Classes e objetos Java - classe Produto

---
+++ Java/Classes e Objetos/Loja e produto/Produto.java
@@ -0,0 +1,19 @@
+public class Produto {
+    // Atributos da classe Produto
+    String nome;
+    Loja vendedor;
+
+    // Este construtor usa um objeto da classe Loja para encapsular
+    // outros atributos(nome da loja)
+    Produto(String nome, Loja vendedor) {
+        this.nome = nome;
+        this.vendedor = vendedor;
+    }
+
+    // Este método usa informações que são de responsabilidade
+    // de outra classe(Loja). Para isso, é necessário criar uma variável do tipo
+    // Loja, para acessar um atributo da mesma
+    String informarProduto(){ 
+        return "A loja " + vendedor.nome + " vende " + nome;
+    }
+}

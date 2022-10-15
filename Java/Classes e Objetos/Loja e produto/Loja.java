c49d8fe587b9f816e58a88d260c1ec941e486920
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2022-10-15 19:45:58 -0300

    Classes e objetos Java - classe Loja

---
+++ Java/Classes e Objetos/Loja e produto/Loja.java
@@ -0,0 +1,20 @@
+public class Loja {
+    // Atributos da classe Loja(aquilo que caracteriza uma loja)
+    String nome;
+    Localizacao local;
+
+    // Construtor da classe Loja, que já instancia um objeto com atributos
+    // Este construtor usa uma outra classe como parâmetro porque esta
+    // encapsula outros atributos(rua e número)
+    Loja(String nome, Localizacao local) {
+        this.nome = nome;
+        this.local = local;
+    }
+
+    // Este método usa objetos de duas classes(Loja e Localizacao)
+    // Assim, é criada uma variável do tipo Localizacao, que, por sua vez,
+    // chama outro método(localizar)
+    String descreverLoja() {
+        return "A loja " + nome + " está localizada em " + local.localizar() + "\n";
+    }
+}

ac141947c9c804f94993e97be6fab68a2e864223
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2022-10-22 15:42:21 -0300

    Classes e objetos Java: classe Hospital(hospital)

---
+++ linguagem_java/classes_e_objetos/hospital/Hospital.java
@@ -0,0 +1,11 @@
+package classes_e_objetos.hospital;
+
+public class Hospital {
+    // Atributo da classe Hospital
+    String nome;
+
+    // Construtor da classe Hospital
+    Hospital(String nome) { 
+        this.nome = nome;
+    }
+}

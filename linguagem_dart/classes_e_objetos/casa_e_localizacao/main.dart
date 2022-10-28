da4e8031d570d8c698d71a1b07b5c6871e042af3
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2022-10-28 20:57:37 -0300

    Classes e objetos Dart: main (casa_e_localizacao)

---
+++ linguagem_dart/classes_e_objetos/casa_e_localizacao/main.dart
@@ -0,0 +1,9 @@
+import 'casa.dart';
+
+void main(List<String> args) {
+  // Objeto
+  Casa casa = Casa(123, "Rua Castro Alves");
+
+  print(
+      "\nA casa de número ${casa.numero} está localizada na ${casa.rua}\n");
+}

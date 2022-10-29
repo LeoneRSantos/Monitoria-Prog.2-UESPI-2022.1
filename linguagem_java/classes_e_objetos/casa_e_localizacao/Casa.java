09867f3fea70a8402ad6192d5d5679f8a224ca15
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2022-10-28 21:04:17 -0300

    Classes e objetos Java: classe Casa (casa_e_localizacao)

---
+++ linguagem_java/classes_e_objetos/casa_e_localizacao/Casa.java
@@ -0,0 +1,14 @@
+package classes_e_objetos.casa_e_localizacao;
+
+public class Casa {
+    // Atributos da classe Casa
+    int numero;
+    String rua;
+
+    // Construtor da classe Casa
+    public Casa(int numero, String rua) {
+        this.numero = numero;
+        this.rua = rua;
+    }
+
+}

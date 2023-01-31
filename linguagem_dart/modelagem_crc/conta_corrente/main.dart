3426b990a2deae8ed967cc0b817162e2208bc76b
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2023-01-31 11:53:36 -0300

    Modelagem CRC Dart: modificação em depositar() no main (conta corrente)

--- linguagem_dart/modelagem_crc/conta_corrente/main.dart
+++ linguagem_dart/modelagem_crc/conta_corrente/main.dart
@@ -1,8 +1,19 @@
 import 'cliente.dart';
 import 'conta_corrente.dart';
+import 'transacao.dart';
 
 void main(List<String> args) {
-  ContaCorrente conta = ContaCorrente(100.0, "123.456-78", Cliente("Cliente 1", "000.000-10"));
+  ContaCorrente conta = ContaCorrente(
+    100.0,
+    "123.456-78",
+    Cliente("Cliente 1", "000.000-10"),
+  );
 
-  print(conta.depositar(conta, 200.0));
+  print(conta.depositar(
+      conta,
+      200.0,
+      Transacao(
+        'Depósito',
+        '13/12/21',
+      )));
 }

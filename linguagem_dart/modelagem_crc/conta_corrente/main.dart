633c2396a1af180546945b5d61004ae94640ea36
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2023-01-30 21:10:05 -0300

    Modelagem CRC Dart: Testes de ContaCorrente em main (conta corrente)

--- linguagem_dart/modelagem_crc/conta_corrente/main.dart
+++ linguagem_dart/modelagem_crc/conta_corrente/main.dart
@@ -1,3 +1,8 @@
+import 'cliente.dart';
+import 'conta_corrente.dart';
+
 void main(List<String> args) {
-  
+  ContaCorrente conta = ContaCorrente(100.0, "123.456-78", Cliente("Cliente 1", "000.000-10"));
+
+  print(conta.depositar(conta, 200.0));
 }

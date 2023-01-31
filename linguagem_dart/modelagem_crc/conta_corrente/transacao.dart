40a0e2db843fb20051d892bcf6ed25bdd1bdfee8
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2023-01-31 11:52:02 -0300

    Modelagem CRC Dart: classe Transacao (conta corrente)

--- linguagem_dart/modelagem_crc/conta_corrente/transacao.dart
+++ linguagem_dart/modelagem_crc/conta_corrente/transacao.dart
@@ -1,3 +1,23 @@
 class Transacao {
-  
+  String _data = "";
+  String _tipo = "";
+
+  // Data
+  String get getData => _data;
+
+  set setData(String data) {
+    _data = data;
+  }
+
+  // Tipo
+  String get getTipo => _tipo;
+
+  set setTipo(String tipo) {
+    _tipo = tipo;
+  }
+
+  Transacao(String tipo, String data) {
+    setTipo = tipo;
+    setData = data;
+  }
 }

32da3bf08012ccc581858c5ecdae24aa3a048fe9
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2023-01-30 21:09:09 -0300

    Modelagem CRC Dart: ContaCorrente (conta corrente)

--- linguagem_dart/modelagem_crc/conta_corrente/conta_corrente.dart
+++ linguagem_dart/modelagem_crc/conta_corrente/conta_corrente.dart
@@ -1,3 +1,48 @@
+import 'cliente.dart';
+
 class ContaCorrente {
-  
+  double _saldo = 0.0;
+  late Cliente _cliente;
+  String _numero = "";
+
+
+  // Saldo
+  double get getSaldo => _saldo;
+
+  set setSaldo(double saldo) {
+    _saldo = saldo;
+  } 
+
+
+  // Cliente
+  Cliente get getCliente => _cliente;
+
+  set setCliente(Cliente cliente) {
+    _cliente = cliente;
+  }
+
+
+  // Número
+  String get getNumero => _numero;
+
+  set setNumero(String numero) {
+    _numero = numero;
+  }
+
+  // Construtor
+  ContaCorrente(double saldo, String numero, Cliente cliente){ 
+    setSaldo = saldo;
+    setNumero = numero; 
+    setCliente = cliente;
+  }
+
+  // Depositar
+  String depositar(ContaCorrente conta, double quantia){ 
+    return "R\$ $quantia foram depositados. \nConta: ${conta.getNumero} \nCliente: ${conta._cliente.getNome} \nSaldo atual: ${conta.getSaldo + quantia}";
+  }
+
+  // Sacar
+  String sacar(ContaCorrente conta, double quantia){ 
+    return "\nR\$ $quantia foram sacados. \nConta: ${conta.getNumero} \nCliente: ${conta._cliente} \nSaldo atual: ${conta.getSaldo + quantia}";
+  }
 }
